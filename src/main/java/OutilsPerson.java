75cb7590318b60def88b13cf384482f181eb3fe6
GaetanMartin <gaetan.martin1993@gmail.com>
2017-04-19 19:56:13 +0200

    Fixed issue

--- src/main/java/OutilsPerson.java
+++ src/main/java/OutilsPerson.java
@@ -7,6 +7,7 @@ import persons.Person;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by p1509413 on 05/04/2017.
@@ -35,17 +36,34 @@ public class OutilsPerson {
      * @return list of persons in range
      */
     public List<IPerson> getPersonsInRangeAge(List<IPerson> persons, GregorianCalendar date, int ageMin, int ageMax) {
-        List<IPerson> returnPersons = new ArrayList<>();
 
         if (ageMin > ageMax) throw new IllegalArgumentException("Age min > age max");
 
+        /* Iterative way
+        List<IPerson> returnPersons = new ArrayList<>();
         for (IPerson person : persons) {
-            if (person.getAge(date) >= ageMin && person.getAge(date) <= ageMax) {
+            int age = person.getAge(date);
+            if (age >= ageMin && age <= ageMax) {
                 returnPersons.add(person);
             }
         }
+        */
 
-        return returnPersons;
+        /* Stream method */
+        return persons.stream()
+                .filter(person-> ageInRange(person.getAge(date), ageMin, ageMax))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Test if  the given age is in the range [minAge, maxAge] given
+     * @param age Age to test
+     * @param ageMin lower bound included
+     * @param ageMax higher bound included
+     * @return true if in range, false else
+     */
+    private static boolean ageInRange(int age, int ageMin, int ageMax) {
+        return age >= ageMin && age <= ageMax;
     }
 
     /**
